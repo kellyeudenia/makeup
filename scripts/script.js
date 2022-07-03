6d3c6124ffc71627a7a872a9626d95e63ec5bf80
vagnerbarbosa <vagnerbd@gmail.com>
2022-07-03 16:33:01 -0300

    organize code

---
+++ scripts/script.js
@@ -0,0 +1,1 @@
+var calcHeight=function(){$("#preview-frame").height($(window).height())};$(document).ready((function(){calcHeight()})),$(window).resize((function(){calcHeight()})).load((function(){calcHeight()}));
